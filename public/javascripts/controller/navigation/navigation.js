76335257c034bfd895b52a1790850bc97cb374d7
sam199510 <273045049@qq.com>
2018-02-05 15:11:35 +0800

    底部授权栏和播放条基础

--- public/javascripts/controller/navigation/navigation.js
+++ public/javascripts/controller/navigation/navigation.js
@@ -1,24 +1,32 @@
 window.onload = function () {
 
+    var oIframe = $(".middle_content iframe");
+
     $(".logo_min").click(function () {
-        window.location.href="index.html";
+        oIframe.attr("src","findMusic.html");
     });
 
     var oNavigationUl = document.getElementById("navigation_ul");
     var oLis = oNavigationUl.getElementsByTagName("li");
 
-    for (var i=0;i<oLis.length;i++){
+    for (var i = 0; i < oLis.length; i++) {
         oLis[i].index = i;
         oLis[i].onclick = function () {
+            for (var j = 0; j < oLis.length; j++) {
+                oLis[j].style.backgroundColor = "#242424";
+                oLis[j].style.color = "#CCCCCC";
+            }
             var index = this.index;
+            oLis[index].style.backgroundColor="#000000";
+            oLis[index].style.color="#FFFFFF";
             if (index == 0) {
-                window.location.href="index.html";
-            } else if(index==1) {
-                window.location.href="myMusic.html";
-            } else if(index==2) {
-                window.location.href="myCollection.html";
+                oIframe.attr("src","findMusic.html");
+            } else if (index == 1) {
+                oIframe.attr("src","myMusic.html");
+            } else if (index == 2) {
+                oIframe.attr("src","myCollection.html");
             } else {
-                window.location.href="settings.html";
+                oIframe.attr("src","settings.html");
             }
         };
     }
