db8b0868e8ad870e82d4f8efb6114edd13996a6a
sam199510 <273045049@qq.com>
2018-03-08 16:47:20 +0800

    单曲页面添加到歌单

--- public/javascripts/controller/style/singleSong.js
+++ public/javascripts/controller/style/singleSong.js
@@ -2,6 +2,12 @@ var oComment = document.getElementById("comment");
 var oCmtBtn = document.getElementById("cmt_btn");
 var oResidueNum = document.getElementById("residue_num");
 
+var oCreateMusicListFrameBg = document.getElementById("create_music_list_frame_bg");
+var oCloseAmlf = document.getElementById("close_amlf");
+var oAddMusicListFrame = document.getElementById("add_music_list_frame");
+
+var addInfo;
+
 oComment.onfocus = function () {
     oComment.onkeydown = function () {
         var cmtValue = oComment.value;
@@ -17,4 +23,17 @@ oComment.onfocus = function () {
 
 oCmtBtn.onclick = function () {
     console.log(oComment.value);
+};
+
+function addTMusicToMusicList(musicId) {
+    oCreateMusicListFrameBg.style.display = "block";
+    oAddMusicListFrame.style.display = "block";
+    addInfo = {musicId: musicId};
+}
+
+
+oCloseAmlf.onclick = function () {
+    oCreateMusicListFrameBg.style.display = "none";
+    oAddMusicListFrame.style.display = "none";
+    addInfo = null;
 };
