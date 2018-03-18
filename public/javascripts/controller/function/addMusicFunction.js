da609b8fac6fe526fc307fbf9ba6fb55d52b4d01
sam199510 <273045049@qq.com>
2018-03-18 17:35:35 +0800

    评论音乐界面评论功能和后台对接成功

--- public/javascripts/controller/function/addMusicFunction.js
+++ public/javascripts/controller/function/addMusicFunction.js
@@ -8,7 +8,7 @@ var oAddMusicListFrame = document.getElementById("add_music_list_frame");
 
 function collectMusicIntoUserMusicList(userMusicListId) {
     $.ajax({
-        url: ipAndHost+'/gdmusicserver/collect/music/into/user/music/list/@collect',
+        url: ipAndHost + '/gdmusicserver/collect/music/into/user/music/list/@collect',
         type: 'POST',
         dataType: 'json',
         data: {
@@ -44,55 +44,59 @@ function collectMusicIntoUserMusicList(userMusicListId) {
 }
 
 function addTheMusicToMusicList(musicId) {
-    oCreateMusicListFrameBg.style.display = "block";
-    oAddMusicListFrame.style.display = "block";
-    addInfo = {musicId: musicId};
-    $.ajax({
-        url: ipAndHost + '/gdmusicserver/find/user/music/list/by/user/id/@query',
-        type: 'GET',
-        dataType: 'json',
-        data: {
-            user_id: $.cookie("uId")
-        },
-        error: function () {
-            customAlert("网络请求错误");
-        },
-        success: function (result) {
-            if (!result.is_success) {
-                customAlert(result.message);
-            } else {
-                var result = result.result;
-                userMusicList = result;
+    if ($.cookie("uId") == null) {
+        customAlert("登录后才能进行收藏哦");
+    } else {
+        oCreateMusicListFrameBg.style.display = "block";
+        oAddMusicListFrame.style.display = "block";
+        addInfo = {musicId: musicId};
+        $.ajax({
+            url: ipAndHost + '/gdmusicserver/find/user/music/list/by/user/id/@query',
+            type: 'GET',
+            dataType: 'json',
+            data: {
+                user_id: $.cookie("uId")
+            },
+            error: function () {
+                customAlert("网络请求错误");
+            },
+            success: function (result) {
+                if (!result.is_success) {
+                    customAlert(result.message);
+                } else {
+                    var result = result.result;
+                    userMusicList = result;
 
-                $("li").remove("#amlf_middle li");
+                    $("li").remove("#amlf_middle li");
 
-                $.each(result, function (n, result) {
+                    $.each(result, function (n, result) {
 
-                    var ailImg = $("<img>").attr("src", result.user_music_list_photo);
-                    var ailDIv = $("<div class='amlfm_item_left'></div>");
-                    ailDIv.append(ailImg);
+                        var ailImg = $("<img>").attr("src", result.user_music_list_photo);
+                        var ailDIv = $("<div class='amlfm_item_left'></div>");
+                        ailDIv.append(ailImg);
 
-                    var aitDiv = $("<p class='amlfm_item_title'>" + result.user_music_list_name + "</p>");
-                    var ainDiv = $("<p class='amlfm_item_num'>" + result.num_of_music_in_user_music_list + "首</p>");
+                        var aitDiv = $("<p class='amlfm_item_title'>" + result.user_music_list_name + "</p>");
+                        var ainDiv = $("<p class='amlfm_item_num'>" + result.num_of_music_in_user_music_list + "首</p>");
 
-                    var aiDiv = $("<div class='amlfm_item'></div>");
-                    aiDiv.append(ailDIv);
-                    aiDiv.append(aitDiv);
-                    aiDiv.append(ainDiv);
+                        var aiDiv = $("<div class='amlfm_item'></div>");
+                        aiDiv.append(ailDIv);
+                        aiDiv.append(aitDiv);
+                        aiDiv.append(ainDiv);
 
-                    var aiLink = $("<a></a>").attr("href", "javascript:collectMusicIntoUserMusicList(" +
-                        result.user_music_list_id + ");")
-                        .css("textDecoration", "none");
-                    aiLink.append(aiDiv);
+                        var aiLink = $("<a></a>").attr("href", "javascript:collectMusicIntoUserMusicList(" +
+                            result.user_music_list_id + ");")
+                            .css("textDecoration", "none");
+                        aiLink.append(aiDiv);
 
-                    var amLis = $("<li></li>");
-                    amLis.append(aiLink);
+                        var amLis = $("<li></li>");
+                        amLis.append(aiLink);
 
-                    $("#amlf_middle").append(amLis);
-                });
+                        $("#amlf_middle").append(amLis);
+                    });
+                }
             }
-        }
-    });
+        });
+    }
 }
 
 oCloseAmlf.onclick = function () {
--- public/javascripts/controller/style/singleSong.js
+++ public/javascripts/controller/style/singleSong.js
@@ -16,7 +16,41 @@ oComment.onfocus = function () {
 };
 
 oCmtBtn.onclick = function () {
-    console.log(oComment.value);
+    if ($.cookie("uId") == null) {
+        customAlert("登录后才能进行评论哦");
+    } else {
+        if (oComment.value.length == 0 || oComment.value == "") {
+            customAlert("评论内容不能为空！");
+        } else if (oComment.value.length > 150) {
+            customAlert("评论字数超过限制");
+        } else {
+            $.ajax({
+                url: ipAndHost + '/gdmusicserver/comment/music/@comment',
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    user_id: $.cookie("uId"),
+                    music_id: musicId,
+                    comment_content: oComment.value
+                },
+                error: function () {
+                    customAlert("网络开小差了");
+                },
+                success: function (result) {
+                    if (!result.is_success) {
+                        customAlert(result.message);
+                    } else {
+                        var result = result.result;
+                        customAlert(result.message);
+                        setTimeout(function () {
+                            var link = "singleSong.html?musicId=" + musicId;
+                            window.location.href = link;
+                        }, 3000);
+                    }
+                }
+            });
+        }
+    }
 };
 
 var musicId = getUrlParam("musicId");
@@ -41,8 +75,37 @@ $.ajax({
             $("#ss_name").text(result.music_name);
             $("#ss_author").text(result.artist_name).attr("href", "javascript:toArtist(" + result.artist_id + ");");
             $("#ss_special").text(result.special_name).attr("href", "javascript:toSpecial(" + result.special_id + ");");
-            $("#collect_btn").attr("href","javascript:addTheMusicToMusicList("+result.music_id+");");
+            $("#collect_btn").attr("href", "javascript:addTheMusicToMusicList(" + result.music_id + ");");
 
         }
     }
 });
+
+if ($.cookie("uId") == null) {
+    $("#my_head_photo").attr("src", "/images/headphoto/default_head_photo.png");
+} else {
+    $.ajax({
+        url: ipAndHost + '/gdmusicserver/user/service/info/@get',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            uID: $.cookie("uId")
+        },
+        error: function () {
+            customAlert("网络请求错误，请稍候重试！");
+        },
+        success: function (result) {
+            if (!result.is_success) {
+                customAlert(result.message);
+            } else {
+                var user = result.result;
+                var allImgExt = ".jpg|.jpeg|.bmp|.png";
+                if (user.head_photo.toLowerCase().match(allImgExt) == null) {
+                    $("#my_head_photo").attr("src", "/images/headphoto/default_head_photo.png");
+                } else {
+                    $("#my_head_photo").attr("src", user.head_photo);
+                }
+            }
+        }
+    });
+}
